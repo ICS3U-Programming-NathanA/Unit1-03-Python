d44efafe117f18bd8a6c6e1e38ac7f174f1a3482
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2022-09-14 23:26:10 +0000

    Added Hello World With Style

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created By: Nathan Araujo
+# Date: Sept 14, 2022
+# This Program Displays Hello, World! With Style.
+
+
+def main():
+    print("Hello, World! With Style")
+    print("I am testing my function here")
+
+if __name__ == "__main__":
+    main()
